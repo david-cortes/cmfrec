5a497b0fcafdc2dfbaa0299355e1e2683b4709a9
David Cortes <david.cortes.rivera@gmail.com>
2021-09-07 17:29:37 -0300

    bump version

--- setup.py
+++ setup.py
@@ -186,7 +186,7 @@ if (force_openblas):
 setup(
     name  = "cmfrec",
     packages = ["cmfrec"],
-    version = '3.2.2',
+    version = '3.2.2-1',
     description = 'Collective matrix factorization',
     author = 'David Cortes',
     author_email = 'david.cortes.rivera@gmail.com',
